525179f77121fa2d150b2698e6a3f0850a353821
Abhijith S <abhijithsasikumar007@gmail.com>
2024-07-08 14:59:30 +0530

    initial commit

---
+++ custom/training/trainingbackoffice/gensrc/org/trainingbackoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 3, 2024, 11:46:08 AM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.trainingbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ custom/training/trainingfulfilmentprocess/gensrc/de/hybris/training/fulfilmentprocess/constants/GeneratedTrainingFulfilmentProcessConstants.java
@@ -0,0 +1,35 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Jul 3, 2024, 11:46:08 AM                    ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2024 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.training.fulfilmentprocess.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "trainingfulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedTrainingFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
